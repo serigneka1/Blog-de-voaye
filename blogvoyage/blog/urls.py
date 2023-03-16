1f40d830787932a9de1ea45a1d0797aa26f51a24
serigneka1 <kaserigne69@gmail.com>
2023-03-16 23:19:39 +0100

    premier commit pour mon blog voyage

---
+++ blogvoyage/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('<slug>:slug/', views.detail, name= 'post_detail' ),
+
+    ]
+
+
+
---
+++ blogvoyage/page/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from blog.models import Post
+# Create your views here.
+
+def frontpage(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'page/frontpage.html', {'posts':posts})
+
+def about(request):
+    return render(request, 'page/about.html')
+
