e229340452dbee66cbb7b4aea28f6897ed005400
serigneka1 <kaserigne69@gmail.com>
2023-03-19 14:51:41 +0100

    ajouté bouton recherche et uploader des images

--- blogvoyage/blog/urls.py
+++ blogvoyage/blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -5,9 +7,5 @@ urlpatterns = [
     path('rechercher/', views.search, name='rechercher'),
     path('<slug:category_slug>/<slug:slug>/', views.detail, name='post_detail'),
     path('<slug:slug>/', views.category, name='category_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-### arrete à 1h21min56s
-# admin
-# fonction rechercher
--- blogvoyage/blog/views.py
+++ blogvoyage/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import CommentForm
 from .models import Post, Category
@@ -13,7 +14,7 @@ def detail(request, category_slug, slug):
             comment.post = post
             comment.save()
 
-            return redirect('post_detail', slug=slug)
+            return redirect('post_detail', category_slug=category_slug, slug=slug)
     else:
         form = CommentForm()
     return render(request, 'blog/detail.html', {'post': post, 'form': form})
@@ -21,10 +22,11 @@ def detail(request, category_slug, slug):
 
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    posts=category.posts.filter(status=Post.ACTIVE)
-    return render(request, 'blog/category.html', {'category': category, 'posts':posts})
+    posts = Post.objects.filter(category=category, status=Post.ACTIVE)
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
 
 def search(request):
-    recherche = request.GET.get("recherche", "")
-    posts = Post.objects.filter(title__icontains=recherche)
-    return render(request, 'blog/search.html', {'posts': posts, 'recherche': recherche})
+    search_query = request.GET.get('q')
+    posts = Post.objects.filter(Q(title__icontains=search_query )|Q(intro__icontains=search_query)|Q(body__icontains=search_query), status=Post.ACTIVE) if search_query else []
+    return render(request, 'blog/search.html', {'posts': posts, 'search_query': search_query})
